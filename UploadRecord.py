95c98b37ba5d6707a9b4eb91bac706a17e8cf59c
LUIS GUILHERME MEDEIROS DE FARIAS <luis.farias@fatec.sp.gov.br>
2018-09-25 19:32:24 -0300

    Formatando entrada dos dados nas listas, convertendo tipos de variaveis

--- UploadRecord.py
+++ UploadRecord.py
@@ -3,9 +3,9 @@
 from selenium import webdriver
 
 '''Antes de tudo, deve-se ter o chrome webdriver baixado e especificado nas
-variaveis de ambiente
+variaveis de ambiente (PATH)
 
-tutorial:
+Tutorial:
 import UploadRecord as up
 up.GetRec('SEU_RA','SUA_SENHA')
 Acontece a magica !
@@ -31,22 +31,24 @@ def GetRec(ra, password):
     med = [] # media da disciplina
     freq = [] # frequencia da disciplina
     per = [] # periodo da disciplina
+    ano = [] # ano realizado a disciplina
 
     # Abaixo estou especificando a tabela do historico curricular e salvando nas listas os parametros
 
     hist = driver.find_element_by_xpath('//*[@id="tbl_hist"]/tbody/tr/td/div/center[1]/table/tbody/tr/td/table[1]/tbody')
     for row in hist.find_elements_by_xpath(".//tr"):
         table = [td.text for td in row.find_elements_by_tag_name('td')]
-        subj_sem.append(table[0])
+        subj_sem.append(int(table[0]))
         cod.append(table[1])
         subj.append(table[2])
-        cred.append(table[7])
-        med.append(table[8])
-        freq.append(table[9])
-        per.append(table[10])
+        cred.append(int(table[7]))
+        med.append(table[8].replace(',', '.'))
+        freq.append(table[9].replace(',', '.'))
+        per.append(int(table[10]))
+        ano.append(int(table[11]))
 
     driver.switch_to_default_content() # saindo do frame
     driver.find_element_by_xpath('//*[@id="div_fsInterno"]/table/tbody/tr[1]/td[3]').click() # disconectando do portal
     driver.close() # encerrando o drive
 
-    return subj_sem, subj, cod, cred, med, freq, per
+    return subj_sem, subj, cod, cred, med, freq, per, ano
